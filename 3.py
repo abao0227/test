45433e83fe5592af23131b406813ce0481b4ca38
abao <wwwrenyubao@126.com>
2016-12-29 15:45:36 +0800

    全部提交

--- 3.py
+++ 3.py
@@ -1,7 +1,28 @@
+# -*- coding=utf8 -*-
 import requests
-req = requests.get('https://www.baidu.com')
-requests.
-print req.headers['content-type']
-print req.encoding
-print req.content
-#print req.text
+from bs4 import BeautifulSoup
+import json
+
+json_string = {
+    "success": "1",
+    "info": "成功",
+    "data": {
+        "list": [
+            {
+                "word": "淘宝seo",
+                "total": 239
+            },
+            {
+                "word": "哪里学seo",
+                "total": 0
+            }
+        ]
+    }
+}
+
+s = json.loads('{"name":"test", "type":{"name":"seq", "parameter":["1", "2"]}}')
+print s
+print s.keys()
+print s["name"]
+print s["type"]["name"]
+print s["type"]["parameter"][1]
